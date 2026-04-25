7a58b0d7319b2db4ad75c921e830dfc3794bf621
shehla <13743@holbertonstudents.com>
2026-04-25 07:05:28 +0000

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,13 @@ def roman_to_int(roman_string):
         return 0
 
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000 }
-    
+
     total = 0
     prev_value = 0
-    
 
     for char in reversed(roman_string):
         current_value = roman_dict.get(char, 0)
-        
+
         if current_value < prev_value:
             total -= current_value
         else:
