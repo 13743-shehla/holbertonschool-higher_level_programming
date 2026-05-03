f5459f4bc6c2fd5fa0c3cc4f35f210893a39707a
shehla <13743@holbertonstudents.com>
2026-05-03 09:54:57 +0000

    task 8

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a search API with a letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {'q': q}
+
+    try:
+        r = requests.post(url, data=payload)
+        json_data = r.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
