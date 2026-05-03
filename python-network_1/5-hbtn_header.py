15e723423359bd1ff07a6bdecaf5bd2c8e6c2b16
shehla <13743@holbertonstudents.com>
2026-05-03 10:11:30 +0000

    task 5

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-bu modul 'requests' paketi vaistesile arqument kimi verilen URLe
+bu modul 'requests' paketi vaisteseile arqument kimi verilen URLe
 sorgu gonderir ve header deki 'X-Request-Id' deyerinin cap edir
 """
 import requests
@@ -9,9 +9,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
     r = requests.get(url)
-    
     request_id = r.headers.get('X-Request-Id')
-    
     print(request_id)
