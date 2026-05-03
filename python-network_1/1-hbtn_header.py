5bea8a1c8564a4e1eb4585057d0678d58fc3da00
shehla <13743@holbertonstudents.com>
2026-05-03 10:07:49 +0000

    task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,12 +2,13 @@
 """
 bu modul verilmis URL den X-Request-Id deyerini oxuyur
 """
-import urllib.request
 import sys
+import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     with urllib.request.urlopen(url) as response:
         request_id = response.headers.get('X-Request-Id')
         print(request_id)
